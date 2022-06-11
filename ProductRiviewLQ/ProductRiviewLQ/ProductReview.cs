1a2832fe952eefcccdf420e2621e2e828bfa8959
Manasisaner <snehasonwane01@gmail.com>
2022-06-11 08:39:50 +0530

    [Manasi]: Create List of variable

---
+++ ProductRiviewLQ/ProductRiviewLQ/ProductReview.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+class ProductReview
+{
+    public int ProducID { get; set; }
+    public int UserID { get; set; }
+    public double Rating { get; set; }
+    public string Review { get; set; }
+    public bool isLike { get; set; }
+}
