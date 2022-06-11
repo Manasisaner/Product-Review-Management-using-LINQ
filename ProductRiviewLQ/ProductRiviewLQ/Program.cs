e434c72dc6ddd215e60c8ee80cbcfcfabaf1b081
Manasisaner <snehasonwane01@gmail.com>
2022-06-11 09:23:27 +0530

    [Manasi]: Skip Top Five Records

--- ProductRiviewLQ/ProductRiviewLQ/Program.cs
+++ ProductRiviewLQ/ProductRiviewLQ/Program.cs
@@ -31,3 +31,6 @@ management.RetrieveCountOfRecords(productReviewList);
 //UC5
 management.RetriveProductIdAndReview(productReviewList);
 Console.ReadLine();
+//UC6
+management.SkipRecord(productReviewList);
+Console.ReadLine();
